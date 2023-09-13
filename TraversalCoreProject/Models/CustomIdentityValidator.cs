639b8937824111cc76eb3a84d15b146debb2c198
Gizem <120684132+Sun-Leo@users.noreply.github.com>
2023-09-13 22:40:08 +0300

    CustomIdentityValidator

---
+++ TraversalCoreProject/Models/CustomIdentityValidator.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace TraversalCoreProject.Models
+{
+	public class CustomIdentityValidator: IdentityErrorDescriber
+	{
+		// Identity kütüphanesinin şifre kurallarını Türkçeleştirmek için IdentityErrorDescriber kullanılır.
+		public override IdentityError PasswordTooShort(int length)
+		{
+			return new IdentityError()
+			{
+				Code= "PasswordTooShort",
+				Description=$"Parola minimum {length} karakter olmalıdır."
+			};
+		}
+	}
+}
